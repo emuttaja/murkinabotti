b146e6f66bf1d2be88d8a8b4aa9d2434ca955387
emuttaja <eliasahok@gmail.com>
2022-06-15 11:07:53 +0300

    Add lunch_fetcher module

---
+++ lunch_fetcher.py
@@ -0,0 +1,74 @@
+import json
+import requests
+import datetime
+
+HERZI = "https://www.sodexo.fi/ruokalistat/output/weekly_json/111"
+REAKTORI = "https://www.foodandco.fi/modules/json/json/Index?costNumber=0812&language=fi"
+NEWTON = "https://fi.jamix.cloud/apps/menuservice/rest/haku/menu/93077/6?lang=fi"
+
+RESTAURANTS = [HERZI, REAKTORI, NEWTON]
+
+
+
+
+
+def reaktori():
+    """ Fetches the lunch data from a json
+
+    Returns
+    -------
+    list
+        list of food components
+    """
+    # get current date
+    now = datetime.datetime.now()
+    today = now.strftime("%Y-%m-%d")
+
+    # open json file
+    response = requests.get(REAKTORI)
+    lunch_json = json.loads(response.text)
+    
+    # navigate the json
+    menus = lunch_json["MenusForDays"]
+    for day in menus:
+        if day["Date"][0:10] == today:
+            return day["SetMenus"][2]["Components"]
+
+
+def newton():
+    """Fetches the lunch data from a json
+
+    Returns
+    -------
+    list
+        list of food components
+    """
+    # get current date
+    now = datetime.datetime.now()
+    today = now.strftime("%Y%m%d")
+
+    # open json file
+    response = requests.get(NEWTON)
+    lunch_json = json.loads(response.text)
+
+    # navigate the json
+    days = lunch_json[0]["menuTypes"][0]["menus"][0]["days"]
+    for day in days:
+        if str(day["date"]) == today:
+            items = day["mealoptions"][0]["menuItems"]
+            components = []
+            for item in items:
+                components.append(item["name"])
+
+    return components
+
+
+def get_lists():
+    """Returns a dictionary of restaurants food lists
+
+    Returns
+    -------
+    dict
+        dictionary of restaurant menus
+    """
+    return {reaktori: reaktori(), newton: newton()}
